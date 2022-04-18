6b54e8e84111fbb06c099f3b168015a29a4f95f5
Rupchand Awale <suraj@redxcel.com>
2022-04-18 10:26:06 +0530

    compact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ResourceComponent } from './resource/resource.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ResourceComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
